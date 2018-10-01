737a342906da35be3509b26a75a22c6719da3a7c
hunterluker <hunterluker1992@gmail.com>
2018-10-01 16:02:25 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Funday.com</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

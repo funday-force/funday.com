6fc19f2271c60947dbfda5453585d479b4ed3d84
Jace H <Jaceholdaway@gmail.com>
2018-10-02 14:34:09 -0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import Side_Navbar from './side-navbar'
-import Header from './Header.js'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from "react";
+import Side_Navbar from "./side-navbar";
+import Header from "./Header.js";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import "./App.css";
 
 // COMPONENTS
-import Homepage from './Components/Homepage/Homepage';
+import Homepage from "./Components/Homepage/Homepage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      {/* <h1>Funday</h1> */}
-      <Header />
-      <Side_Navbar />
+        {/* <h1>Funday</h1> */}
+        <Header />
+        <Side_Navbar />
         <Homepage />
       </div>
     );

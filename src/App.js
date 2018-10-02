d37879101242558fed138458dcbe31b86715098b
Jace H <Jaceholdaway@gmail.com>
2018-10-02 15:34:18 -0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
-import Side_Navbar from "./side-navbar";
-import Header from "./Header.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
 import "./App.css";
+import routes from "./routes";
 
 // COMPONENTS
 import Homepage from "./Components/Homepage/Homepage";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        {/* <h1>Funday</h1> */}
-        <Header />
-        <Side_Navbar />
-        <Homepage />
-      </div>
-    );
+    return <div className="App">{routes}</div>;
   }
 }
 

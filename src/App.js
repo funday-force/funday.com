dd8ac9011b6160fa5852157419352b06f2730fa3
Jace H <Jaceholdaway@gmail.com>
2018-10-02 11:30:59 -0600

    jh-navbar complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Side_Navbar from './side-navbar'
+import Header from './Header.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import './App.css';
 
@@ -6,7 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Funday.com</h1>
+      {/* <h1>Funday</h1> */}
+      <Header />
+      <Side_Navbar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HashRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>
+,document.getElementById('root'));
